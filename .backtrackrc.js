32449871292d0f75a54f98b5f856c64bde30fe37
Chris Blossom <cbloss@gmail.com>
2019-01-14 19:45:26 -0800

    remove excess backtrack settings

--- .backtrackrc.js
+++ .backtrackrc.js
@@ -3,22 +3,11 @@
 module.exports = {
     presets: [['@backtrack/node-module', { typescript: true }]],
 
-    packageJson: {
-        /**
-         * test.watch doesn't work nicely with e2e tests
-         */
-        scripts: {
-            'test.watch': 'jest --watch src',
-            'test.watch-e2e': 'jest --watch e2e-tests',
-        },
-    },
-
     config: {
         wallaby: (config) => {
-            // config.files.push('!src/**');
             // config.tests.push('!src/**');
-            config.files.push('!e2e-tests/**');
-            config.tests.push('!e2e-tests/**');
+            // config.files.push('!e2e-tests/**');
+            // config.tests.push('!e2e-tests/**');
 
             return config;
         },
