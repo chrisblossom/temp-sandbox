06b47f4d55d21195ec974529f37670f373754fe6
Chris Blossom <cbloss@gmail.com>
2019-01-28 13:53:48 -0800

    remove coverage report from del.ts

--- .backtrackrc.js
+++ .backtrackrc.js
@@ -4,6 +4,10 @@ module.exports = {
     presets: [['@backtrack/node-module', { typescript: true }]],
 
     config: {
+        jest: {
+            coveragePathIgnorePatterns: ['<rootDir>/src/utils/del.ts'],
+        },
+
         wallaby: (config) => {
             // config.tests.push('!src/**');
             // config.files.push('!e2e-tests/**');
