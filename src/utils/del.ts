9793379f7d155805193dbc6379713249b81943dc
Chris Blossom <cbloss@gmail.com>
2019-07-11 15:04:51 -0700

    update del wrapper to correctly return files when EINVAL happens

--- src/utils/del.ts
+++ src/utils/del.ts
@@ -1,17 +1,17 @@
 import delActual, { sync as DelSync, Options } from 'del';
 
-async function waitForMs(ms: number): Promise<void> {
-	return new Promise((resolve): void => {
-		setTimeout(resolve, ms);
-	});
-}
-
 async function del(
 	patterns: string | readonly string[],
-	options?: Options,
+	options: Options = {},
 ): ReturnType<typeof delActual> {
+	// workaround for https://github.com/sindresorhus/del/issues/68
+	const getFilesToRemove = await delActual(patterns, {
+		...options,
+		dryRun: true,
+	});
+
 	try {
-		const removed = await delActual(patterns, options);
+		const removed = await delActual(getFilesToRemove, options);
 
 		return removed;
 	} catch (error) {
@@ -19,26 +19,12 @@ async function del(
 		 * Intermittently error code 'EINVAL' is being thrown here.
 		 * Would be great to find out why
 		 * Test will fail only if validating removed files.
-		 * Retry twice with 100ms between each try
 		 */
 		if (error.code === 'EINVAL') {
-			await waitForMs(100);
+			// when the EINVAL happens, the files are usually removed 90% of the time. Run delActual again to get the remaining 10%
+			await delActual(getFilesToRemove, options);
 
-			try {
-				const removed = await delActual(patterns, options);
-
-				return removed;
-			} catch (error2) {
-				await waitForMs(100);
-
-				if (error2.code === 'EINVAL') {
-					const removed = await delActual(patterns, options);
-
-					return removed;
-				}
-
-				throw error2;
-			}
+			return getFilesToRemove;
 		}
 
 		throw error;
